8cd538c59a7933533913441417d42cf99948c4ed
Chad <spizeck@zoho.com>
2016-02-15 20:41:38 -0900

    Working game.

--- number-game.py
+++ number-game.py
@@ -3,14 +3,13 @@ import random
 
 class NumberGame:
     """
-    Generates the high number
+    NumberGame core
     """
 
-    def __init__(self, highNumber, playerName):
-        self.highNumber = highNumber
-        self.playerName = playerName
-        self.com_number = random.randint(1, self.highNumber)
-        self.check_win = False
+    def __init__(self, high_number, player_name):
+        self.high_number = high_number
+        self.player_name = player_name
+        self.com_number = random.randint(1, self.high_number)
         self.play_again = 'n'
         self.guess = None
         self.guess_count = 1
@@ -19,79 +18,75 @@ class NumberGame:
         """
         Starts the game
         """
-        self.check_win = False
         self.play_again = 'n'
-        self.guess_count = 1
-        self.gameLoop()
+        self.guess_count = 0
+        self.greeting()
+        self.game_loop()
 
-    def gameLoop(self):
+    def generate_num(self):
+        self.com_number = random.randint(1, self.high_number)
+        self.guess_count = 0
+
+    def greeting(self):
+        print('Well {}, I am thinking of a number between 1 and {}.'.format(self.player_name, self.high_number))
+
+    def game_loop(self):
         """
         Loops the game
         """
-        print('Well {}, I am thinking of a number between 1 and {}.'.format(self.playerName, self.highNumber))
         # 6 valid guesses allowed
-        while self.guess is not self.com_number and self.guess_count < 6:
-            # Loop in case of invalid input
-            while True:
-                # Get input and check for valid input
-                try:
-                    self.guess = int(input('Take a guess.\n'))
-                    if 1 <= self.guess <= self.highNumber:
-                        break
-                    else:
-                        print('Invalid input.')
-                except ValueError:
-                    print('Invalid input')
-            # Add 1 to guess count
-            self.guess_count += 1
-            # Give user clues
-            if self.guess < self.com_number:
-                print('Too low.')
-            elif self.guess > self.com_number:
-                print('Too high.')
-        # set check_win flag to True
-        self.check_win = True
-        self.playAgain()
+        # Loop in case of invalid input
+        while True:
+            # Get input and check for valid input
+            try:
+                self.guess = int(input('Take a guess.\n'))
+                if 1 <= self.guess <= self.high_number:
+                    break
+                else:
+                    print('Invalid input.')
+            except ValueError:
+                print('Invalid input')
+        # Add 1 to guess count
+        self.guess_count += 1
+        # Give user clues
+        if self.guess < self.com_number and self.guess_count < 6:
+            print('Too low.')
+            self.game_loop()
+        elif self.guess > self.com_number and self.guess_count < 6:
+            print('Too high.')
+            self.game_loop()
+        elif self.guess is self.com_number:
+            self.you_win()
+            self.high_number += 20
+            self.again()
+        else:
+            print('Nice try. My number was {}.'.format(self.com_number))
+            self.high_number = 20
+            self.again()
 
-    def checkWin(self):
-        """
-        Returns value for play_again loop in main()
-        """
-        return self.check_win
+    def you_win(self):
+        print('Way to go {}! You guessed my number in {} guesses.'.format(self.player_name, self.guess_count))
 
-    def playAgain(self):
+    def again(self):
         """
         Check if they want to play again.
         """
         self.play_again = input('Do you want to play again?\n(y/n)')
-        if self.play_again != 'y':
-            return False
+        if self.play_again == 'y':
+            self.generate_num()
+            self.greeting()
+            self.game_loop()
         else:
-            return True
-
-
-def highScore():
-    """
-    Prints the top 3 to be added later
-    """
-    pass
+            print('Thanks for playing {}!'.format(self.player_name))
+            return
 
 
 def main():
     difficulty = 1
-    highNumber = 20 * difficulty
-    playerName = input('Hello! What is your name?\n')
-    game = NumberGame(highNumber, playerName)
+    high_number = 20 * difficulty
+    player_name = input('Hello! What is your name?\n')
+    game = NumberGame(high_number, player_name)
     game.start()
-    while game.playAgain() is True:
-        if game.checkWin() is True:
-            difficulty += 1
-        else:
-            difficulty = 1
-        highNumber = 20 * difficulty
-        game = NumberGame(highNumber, playerName)
-        game.start()
-    print('Thanks for playing!')
 
 
 # Executes code for testing
